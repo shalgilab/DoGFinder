06d76d2378e7b2347502d3bc3f46c40b489d0282
shalgilab <34984563+shalgilab@users.noreply.github.com>
2018-05-22 16:04:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,30 @@ import os
 from setuptools import setup
 from distutils.core import setup
 from setuptools.command.install import install
-import commands
 import sys
 
 class CustomInstallCommand(install):
     """Customized setuptools install command - prints a friendly greeting."""
     def run(self):
-        bed_strng=commands.getstatusoutput('bedtools--version')
-        if bed_strng[1].find("command not found")!=-1:
-            print "Warning: Couldn't find bedtools on path"
-        sam_strng=commands.getstatusoutput('samtools')
+
+        python_ver=sys.version
+        if python_ver[0]=='3':
+            print ("Error: you are using python 3, should be using python 2")
+            sys.exit()
+        bed_strng=os.popen('bedtools --version').read()
+        if bed_strng.find("command not found")!=-1:
+            print ("Warning: Couldn't find bedtools on path")
+        sam_strng=os.popen('samtools').read()
         
-        if sam_strng[1].find("command not found")!=-1:
-            print "Warning: Couldn't find samtools on path"
+        if sam_strng.find("not found")!=-1:
+            print ("Warning: Couldn't find samtools on path")
         install.run(self)
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
         
 setup(name='DoGFinder',
-      version='1.0.0',
+      version='1.0.1',
       description='DoGs pipeline',
       install_requires=['pybedtools','numpy','pandas','pysam','RSeQC'],
       scripts=['Get_loci_annotation','Get_DoGs','Get_DoGs_rpkm','Pre_Process','Union_DoGs_annotation','Common_DoGs_annotation'],
